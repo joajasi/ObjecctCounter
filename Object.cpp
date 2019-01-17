4dd628766c8475b0d28a5b8e84a70f4a80a2f7ed
joajasi <joajasi@gmail.com>
2019-01-17 22:52:19 +0100

    Little corrections

--- Object.cpp
+++ Object.cpp
@@ -5,10 +5,10 @@ int Object::counter = 0;
 
 Object::Object() {
 	std::cout << "Object created" << std::endl;
-	std::cout << "Objects: " << ++counter << std::endl;
+	std::cout << "Number of objects: " << ++counter << std::endl;
 }
 
 Object::~Object() {
 	std::cout << "Object destroyed" << std::endl;
-	std::cout << "Objects: " << --counter << std::endl;
+	std::cout << "Number of objects: " << --counter << std::endl;
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
+#include <iostream>
 #include "Object.h"
+using namespace std;
 
 int main() {
+	cout << "Main: create object a" << endl;
 	Object a; 
+	cout << "Main: create object b" << endl;
 	Object* b = new Object;
+	cout << "Main: delete object b" << endl;
 	delete b; 
+	cout << "Main create object c" << endl;
 	Object c; 
 	return 0;
 }
